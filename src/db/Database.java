8fbcbc297ef5165fcb0cd50740491a2602fcfffa
Ali <ali.glm484@gmail.com>
2025-03-30 18:52:05 +0330

    first step

---
+++ src/db/Database.java
@@ -0,0 +1,44 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public final class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int id = 0;
+
+    public static void add(Entity e) {
+        e.id = ++id;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) {
+        for(Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == id) {
+                entities.remove(i);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity e) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (e.id == entities.get(i).id) {
+                entities.set(i, e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(e.id);
+    }
+}
